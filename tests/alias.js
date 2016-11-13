bcf104926bb0545cae96f31214624b6727433bfc
michael <4apaev@gmail.com>
2016-11-13 15:10:30 +0200

    check if descriptors are the same

--- tests/alias.js
+++ tests/alias.js
@@ -1,12 +1,22 @@
 const is = require('is')
+const declare = require('declare')
 
 describe('alias',  () => {
 
   const test = (Ctor, dict) => {
     dict.forEach(x => {
       let [ alias, method ] = x.split(':');
-      let msg = `${ Ctor.name }.prototype.${ method } should have alias "${ alias }"`
-      it(msg, () => is.own.assert(Ctor.prototype, alias, msg))
+
+      it(`${ Ctor.name }.prototype.${ method } should have alias "${ alias }"`, () => {
+        is.own.assert(Ctor.prototype, alias)
+      })
+
+      it(`"${ method }" descriptor should be the same as "${ alias }" descriptor`, () => {
+        let a = declare.getDesc(Ctor.prototype, method)
+        let b = declare.getDesc(Ctor.prototype, alias)
+        is.equal.assert(a, b)
+      })
+
     })
   }
 
